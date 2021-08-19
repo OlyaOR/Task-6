2652b2dc90e6b8e93d565558085de377125d9a03
Olga Rozhkova <olga.gavrilova.og94@gmail.com>
2021-08-19 20:39:20 +0300

    add info text on main page

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,9 @@
 const Main = () => (
     <section className='main'>
         <h1>Главная страница</h1>
+        <p className='main_text'>Этот сайт является результатом выполнения тестового задания (задача №6) от компании Resliv. <br />
+        С текстом задания можно ознакомиться <a target='blank' href="https://tasks-for-interview-resliv.netlify.app/">здесь</a>.
+        </p>
     </section>
 );
   
